a208786141cb018203d408d51d58a65df62cfae0
Burhana786 <119918194+Burhana786@users.noreply.github.com>
2025-11-05 14:43:54 +0530

    sitemap_updation_2

--- kif_realty/settings.py
+++ kif_realty/settings.py
@@ -128,14 +128,14 @@ DEFAULT_FROM_EMAIL = 'KIF Realty <noreply@kifrealty.com>'
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.getenv('DB_NAME'),
-    #     'USER': os.getenv('DB_USER'),
-    #     'PASSWORD': os.getenv('DB_PASSWORD'),
-    #     'HOST': os.getenv('DB_HOST', 'localhost'),
-    #     'PORT': os.getenv('DB_PORT', '5432'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
      
 }
 
